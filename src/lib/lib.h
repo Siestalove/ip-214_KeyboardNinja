354aba8f20d53756b3f6a820d2b66b31dee91efe
jigi01 <gleb_zh@icloud.com>
2024-05-21 16:22:17 +0700

    update h

--- src/lib/lib.h
+++ src/lib/lib.h
@@ -7,3 +7,8 @@
 #define MAX_NUM_WORDS 10000
 
 void print_word(const char* word, const char* input);
+void play_game(
+        char words[MAX_NUM_WORDS][MAX_WORD_LENGTH],
+        int min_len,
+        int max_len,
+        int num_rounds);
